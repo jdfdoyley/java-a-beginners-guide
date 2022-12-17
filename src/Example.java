fe3061cf57fc6496b1e2165982c6a344b8f56c36
Jason D'Oyley <jdfdoyley@gmail.com>
2022-12-17 08:08:08 -0500

    Demo a simple Java program

---
+++ src/Example.java
@@ -0,0 +1,13 @@
+/*
+* Jason D. F. D'Oyley
+* Dec 17, 2022
+*
+* This is a simple Java program.
+* */
+
+public class Example {
+    // A Java program begins with a call to main()
+    public static void main(String[] args) {
+        System.out.println("Java drives the web.");
+    }
+}
